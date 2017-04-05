9b6f9990b67c2ac0cd558850532e2ba5500d08f6
Ezra Christian W <echruz1412@gmail.com>
2017-04-05 14:46:07 +0700

    make it better

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,15 @@
 
 ActiveRecord::Schema.define(version: 20170405043501) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "alamats", force: :cascade do |t|
     t.string   "address"
     t.integer  "contact_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["contact_id"], name: "index_alamats_on_contact_id"
+    t.index ["contact_id"], name: "index_alamats_on_contact_id", using: :btree
   end
 
   create_table "contacts", force: :cascade do |t|
@@ -32,7 +35,7 @@ ActiveRecord::Schema.define(version: 20170405043501) do
     t.integer  "contact_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["contact_id"], name: "index_phones_on_contact_id"
+    t.index ["contact_id"], name: "index_phones_on_contact_id", using: :btree
   end
 
 end
