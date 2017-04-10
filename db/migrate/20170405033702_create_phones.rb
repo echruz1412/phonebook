ded08cb269d92be7a69cdcb4e6d7029a2a7aba42
Ezra Christian W <echruz1412@gmail.com>
2017-04-10 11:27:27 +0700

    make it better

--- db/migrate/20170405033702_create_phones.rb
+++ db/migrate/20170405033702_create_phones.rb
@@ -2,8 +2,8 @@ class CreatePhones < ActiveRecord::Migration[5.0]
   def change
     create_table :phones do |t|
       t.integer :nphone,
-      t.text :status
-      t.references :contact, foreign_key: true
+      t.text :status,
+      t.references :contact, foreign_key: true,
 
       t.timestamps
     end
